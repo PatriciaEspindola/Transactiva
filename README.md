0e203b534d318193a96c56e363418f7a54bbc97f
Patricia <patricia.egu@outlook.com>
2025-06-22 23:10:22 +0000

    Add my view and my route site

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Transactiva
 Desarrollo de un sistema de pagos. Este incluirá un simulador en el que se podrá realizar un pago eligiendo entre distintos métodos disponibles (transferencia, depósito o PayPal). El sistema estará diseñado guardar cada transacción dependiendo del método de pago seleccionado.
+## Pasos para instalar mi proyecto 
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
+// GET my view
+router.get("/my-page", function (req, res, next) {
+  const myData = {
+    name: "Paty",
+    title: "My Personal site",
+    say: "hello",
+  };
+  res.render("myview", { ...myData });
+});
 module.exports = router;
